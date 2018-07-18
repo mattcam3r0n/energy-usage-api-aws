61b4cdebfb8e56e4bf92f254585b7ca4de685409
Matt Cameron <cameron.matt@gmail.com>
2018-07-17 19:59:04 -0500

    required authorization to api endpoints

--- currentUsage.js
+++ currentUsage.js
@@ -1,4 +1,5 @@
 import Egauge from './Egauge';
+import { success, failure } from "./lib/response-helper";
 
 // eslint-disable-next-line import/prefer-default-export
 export const getCurrentUsage = (event, context, callback) => {
@@ -6,22 +7,11 @@ export const getCurrentUsage = (event, context, callback) => {
   eg.getInstantaneousData()
     .then(mapData)
     .then((data) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(data)
-      });
+      callback(null, success(data));
     })
-    .catch((e) => callback(e));
+    .catch(((e) => callback(null, failure({ status: false }))));
+//    .catch((e) => callback(e));
 };
-
-export function current(request, response) {
-  const eg = new Egauge();
-  eg.getInstantaneousData()
-    .then(mapData)
-    .then((data) => {
-      response.json(data);
-    });
-}
 
 function mapData(data) {
   const excludedCategories = ['Grid', 'Solar +'];
